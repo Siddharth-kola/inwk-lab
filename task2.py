3b18e7bed0b05e3040bef9bed87a47b89aaa39f3
siddharth <sd632094@dal.ca>
2020-05-11 17:01:53 -0300

    second commit for printing word frequency

--- task2.py
+++ task2.py
@@ -1,22 +1,25 @@
-  GNU nano 4.9.2                                                                     task2.py
+
+#task 2
 import string
 list1=[]
-book=open("pg25990.txt")
-for line in book:
-
-    for words in line.split():
-        #print(words.strip(string.punctuation).lower())
-        list1.append(words)
-
-print(len(list1))
-
-import string
-list1=[]
+dict1={}
 book=open("pg25990.txt")
 for line in book:
    
     for words in line.split():
         #print(words.strip(string.punctuation).lower())
-        list1.append(words)
+        list1.append(words.strip(string.punctuation).lower())
+        if words in dict1:
+            count+=1
+            dict1.update({words:count})
+            
+        else:
+            count=1
+            dict1.update({words:count})
         
-print(len(list1)) 
+print(len(list1))    
+#print(dict1)
+
+for i in sorted(dict1.values(),reverse=True)[:20]:
+       print(i)
+
