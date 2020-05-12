071edde89a37746dd852a3f0c7daa9fabd4c0b2e
siddharth <sd632094@dal.ca>
2020-05-12 13:10:55 -0300

    this is final commit for task 3

--- task3.py
+++ task3.py
@@ -2,10 +2,8 @@
 
 import string
 dict1={}
-book=open("pg25990.txt")
-
-
-
+book=open("pg25990.txt")  
+  
 for line in book:
    
     for words in line.split():
@@ -13,25 +11,44 @@ for line in book:
        
         if words in dict1:
             count+=1
-            dict1.update({count:words})
+            dict1.update({words.strip(string.punctuation):count})
             
         else:
             count=1
-            dict1.update({count:words})
-            
-            
-            
+            dict1.update({words.strip(string.punctuation):count})
         
    
 
-print(dict1)
+#print(dict1)
+dict2={}
+dict3={}
+count=1
+for i  in sorted(dict1.values(),reverse=True):
+  #print(i)
+  
+  dict3.update({i:count})
+  count+=1
+print(dict3)
+
+import math
 
 
+import matplotlib.pyplot as pyplot
+pyplot.clf()
+#pyplot.xscale(2000)
+#pyplot.yscale(2000)
+pyplot.title('word frequency & rank')
+pyplot.xlabel('frequency')
+pyplot.ylabel('rank')
+
+list4=[]
+list5=[]
+for k,v in dict3.items():
+  list4.append(math.log10(k))
+  list5.append(math.log10(v))
+
+pyplot.plot(list4,list5)
 
 
-for i in sorted(dict1.keys(),reverse=True)[:20]:
-       print(i)
+pyplot.show()
         
-        
-        
-print(dict1[1])
